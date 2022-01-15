9bd6a5b903d4dff024f2c4b328df752c7ec4eebd
Abhijeet Kumar <abhijeetkumar2706@gmail.com>
2022-01-15 21:10:46 +0530

    Updates

---
+++ precache-manifest.031141267fb079e2d397a0108c7504bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849e3da1ba0ddc8177348e616ee06b83",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "991bfb9440fa0c08ebdc",
+    "url": "/photo-gallery/static/css/main.365a5dca.chunk.css"
+  },
+  {
+    "revision": "ec6994015d74bbb53fa6",
+    "url": "/photo-gallery/static/js/2.0b28303d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/photo-gallery/static/js/2.0b28303d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991bfb9440fa0c08ebdc",
+    "url": "/photo-gallery/static/js/main.68ae0aa6.chunk.js"
+  },
+  {
+    "revision": "0e98923ae2b7c412e111",
+    "url": "/photo-gallery/static/js/runtime-main.ebe8cd21.js"
+  }
+]);
